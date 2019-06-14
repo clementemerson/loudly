755cd046bb594e1b396c7ac561724625b88b886a
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-06-14 17:18:08 +0530

    Add files via upload

--- lib/resources/rest/login_service.dart
+++ lib/resources/rest/login_service.dart
@@ -1,1 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+import 'network_helper.dart';
+
+
+class LoginService {
+  static String serverName = 'https://loudly.loudspeakerdev.net';
+
+  static Future<String> getOTP({@required String phoneNumber}) async {
+    String sessionId = '';
+    try {
+      String requestUrl = serverName + '/getotp/$phoneNumber';
+
+      var data = await NetworkHelper.getData(url: requestUrl);
+      if (data && data.Status) {
+        if (data.Status == 'Success') {
+          sessionId = data.Data;
+        }
+      } else {
+        throw Exception('OTP not generated');
+      }
+    } catch (Exception) {
+      throw Exception('OTP not generated');
+    }
+
+    return sessionId;
+  }
+
+  static Future<String> verifyOTP(
+      {@required String otp, @required String sessionId}) async {
+    String token = '';
+    try {
+      String requestUrl = serverName + '/signin';
+      dynamic body = {"sessionid": sessionId, "otp": otp};
+
+      dynamic data = await NetworkHelper.postData(url: requestUrl, body: body);
+      if (data && data.Status) {
+        if (data.Status == 'Success') {
+          token = data.Data;
+          final storage = new FlutterSecureStorage();
+          await storage.write(key: 'token', value: token);
+        }
+      } else {
+        throw Exception('Failed to verify OTP');
+      }
+    } catch (Exception) {
+      throw Exception('Failed to verify OTP');
+    }
+
+    return token;
+  }
+}
---
+++ lib/resources/rest/network_helper.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class NetworkHelper {
+  static Future getData({@required String url}) async {
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+      throw Exception('Failed to get data');
+    }
+  }
+
+  static Future postData({@required String url, @required dynamic body}) async {
+    http.Response response = await http.post(url, body: body);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+      throw Exception('Failed to post data');
+    }
+  }
+}
