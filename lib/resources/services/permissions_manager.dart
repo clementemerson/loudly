12f8ffb4c8d30ac9107ad9afb6726ddb7726bba9
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-06-17 17:07:39 +0530

    Add files via upload

--- lib/resources/services/permissions_manager.dart
+++ lib/resources/services/permissions_manager.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
+class PermissionManager {
+  static Future<bool> checkPermission(
+      {@required PermissionGroup permissionGroup}) async {
+    PermissionStatus permission = await PermissionHandler()
+        .checkPermissionStatus(permissionGroup);
+    if (permission != PermissionStatus.denied &&
+        permission != PermissionStatus.disabled &&
+        permission != PermissionStatus.granted) {
+      Map<PermissionGroup, PermissionStatus> permissions =
+          await PermissionHandler()
+              .requestPermissions([permissionGroup]);
+      if (permissions[permissionGroup] == PermissionStatus.granted) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
