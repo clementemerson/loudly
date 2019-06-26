f30316dbc5d2e3ec4ec60a95b5bf7100e5886e4d
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-06-26 17:07:48 +0530

    Create database.dart

---
+++ lib/data/database.dart
@@ -0,0 +1,105 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DBProvider {
+  // Create a singleton
+  DBProvider._();
+
+  static final DBProvider db = DBProvider._();
+  Database _database;
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database;
+    }
+
+    _database = await initDB();
+    return _database;
+  }
+
+  initDB() async {
+    // Get the location of our app directory. This is where files for our app,
+    // and only our app, are stored. Files in this directory are deleted
+    // when the app is deleted.
+    Directory documentsDir = await getApplicationDocumentsDirectory();
+    String path = join(documentsDir.path, 'app.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onOpen: (db) async {},
+      onCreate: (Database db, int version) async {
+        // Create the groupinfo table
+        await db.execute('''CREATE TABLE groupinfo(
+              id INTEGER DEFAULT -1, 
+              name TEXT DEFAULT '',
+              desc TEXT DEFAULT '',
+              createdby INTEGER DEFAULT -1,
+              createdAt INTEGER DEFAULT 0,
+              updatedAt INTEGER DEFAULT 0
+              )''');
+
+        // Create the grouppolls table
+        await db.execute('''CREATE TABLE grouppolls(
+              pollid INTEGER DEFAULT -1, 
+              groupid INTEGER DEFAULT -1,
+              sharedby INTEGER DEFAULT -1,
+              createdAt INTEGER DEFAULT 0,
+              updatedAt INTEGER DEFAULT 0
+              )''');
+
+        // Create the groupusers table
+        await db.execute('''CREATE TABLE groupusers(
+              groupid INTEGER DEFAULT -1, 
+              user_id INTEGER DEFAULT -1,
+              addedby INTEGER DEFAULT -1,
+              permission TEXT DEFAULT 'USER'
+              createdAt INTEGER DEFAULT 0,
+              updatedAt INTEGER DEFAULT 0
+              )''');
+
+        // Create the polldata table
+        await db.execute('''CREATE TABLE polldata(
+              id INTEGER DEFAULT -1,
+              title TEXT DEFAULT '', 
+              resultispublic INTEGER DEFAULT 1,
+              canbeshared INTEGER DEFAULT 1,
+              createdby INTEGER DEFAULT -1,
+              createdAt INTEGER DEFAULT 0,
+              updatedAt INTEGER DEFAULT 0
+              )''');
+
+        // Create the pollvotedata table
+        await db.execute('''CREATE TABLE pollvotedata(
+              pollid INTEGER DEFAULT -1, 
+              user_id INTEGER DEFAULT -1,
+              optionindex INTEGER DEFAULT -1,
+              createdAt INTEGER DEFAULT 0,
+              updatedAt INTEGER DEFAULT 0
+              )''');
+
+        // Create the pollvoteregister table
+        await db.execute('''CREATE TABLE pollvoteregister(
+          id INTEGER PRIMARY KEY,
+              pollid INTEGER DEFAULT -1, 
+              user_id INTEGER DEFAULT -1,
+              votetype INTEGER DEFAULT -1,
+              createdAt INTEGER DEFAULT 0,
+              updatedAt INTEGER DEFAULT 0
+              )''');
+
+        // Create the users table
+        await db.execute('''CREATE TABLE users(
+              user_id INTEGER DEFAULT -1,
+              name TEXT DEFAULT '',
+              statusmsg TEXT DEFAULT '',
+              phonenumber TEXT DEFAULT '',
+              createdAt INTEGER DEFAULT 0,
+              updatedAt INTEGER DEFAULT 0
+              )''');
+      },
+    );
+  }
+}
