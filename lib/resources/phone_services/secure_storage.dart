b71ea5549a9527efa9be4ded838f595aa103aa34
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-07-15 14:31:31 +0530

    Create secure_storage.dart

---
+++ lib/resources/phone_services/secure_storage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+/// Gets and Sets keys and values to secure storage
+class SecureStorage {
+  static final String jwtToken = 'jwtToken';
+  static final String selfUser = 'user_id';
+
+  static final SecureStorage _instance = SecureStorage._internal();
+  FlutterSecureStorage _storage;
+  factory SecureStorage() => _instance;
+
+  SecureStorage._internal() {
+    // init things inside this
+    _storage = new FlutterSecureStorage();
+  }
+
+/// Sets keys and values
+  Future<void> write({@required String key, @required String value}) async {
+    await _storage.write(key: key, value: value);
+  }
+
+/// Gets value of key
+  Future<String> read({@required String key}) async {
+    final String value = await _storage.read(key: key);
+    return value;
+  }
+}
