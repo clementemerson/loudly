925504932a8a68521541572161f80708ac7b2699
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-08-20 17:19:37 +0530

    Add files via upload

--- lib/ui/Lists/poll_list.dart
+++ lib/ui/Lists/poll_list.dart
@@ -3,15 +3,15 @@ import 'dart:core';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_circular_chart/flutter_circular_chart.dart';
-import 'package:loudly/models/grouppoll.dart';
+import 'package:loudly/providers/polllist.dart';
+import 'package:loudly/providers/pollopts.dart';
 import 'package:loudly/ui/Screens/pollresult_screen.dart';
 import 'package:loudly/ui/Screens/pollvote_screen.dart';
 
 import 'package:loudly/project_enums.dart';
 import 'package:loudly/project_styles.dart';
 
-import 'package:loudly/models/polldata.dart';
-import 'package:loudly/ui/globals.dart';
+import 'package:provider/provider.dart';
 
 class PollList extends StatefulWidget {
   final PollListType pollListType;
@@ -24,43 +24,42 @@ class PollList extends StatefulWidget {
 }
 
 class _PollListState extends State<PollList> {
-  List<PollData> _pollList = [];
 
   @override
   void initState() {
-    _getPollData();
+    //_getPollData();
 
     super.initState();
   }
 
-/// Gets polls list based on [pollListType] passed.
-  _getPollData() async {
-    try {
-      List<PollData> pollList;
-      switch (widget.pollListType) {
-        case PollListType.All:
-          pollList = await PollData.getAll();
-          break;
-        case PollListType.User:
-          pollList = await PollData.getUserCreatedPolls(Globals.selfUserId);
-          break;
-        case PollListType.Group:
-          List<GroupPoll> groupPolls =
-              await GroupPoll.getAllByGroup(widget.groupId);
-          List<int> pollIds = List.generate(groupPolls.length, (i) {
-            return groupPolls[i].pollid;
-          });
-          pollList = await PollData.getPollDataForPolls(pollIds);
-          break;
-      }
+// /// Gets polls list based on [pollListType] passed.
+//   _getPollData() async {
+//     try {
+//       List<PollData> pollList;
+//       switch (widget.pollListType) {
+//         case PollListType.All:
+//           pollList = await PollData.getAll();
+//           break;
+//         case PollListType.User:
+//           pollList = await PollData.getUserCreatedPolls(Globals.selfUserId);
+//           break;
+//         case PollListType.Group:
+//           List<GroupPoll> groupPolls =
+//               await GroupPoll.getAllByGroup(widget.groupId);
+//           List<int> pollIds = List.generate(groupPolls.length, (i) {
+//             return groupPolls[i].pollid;
+//           });
+//           pollList = await PollData.getPollDataForPolls(pollIds);
+//           break;
+//       }
 
-      setState(() {
-        if (this.mounted) _pollList.addAll(pollList);
-      });
-    } catch (e) {
-      print(e);
-    }
-  }
+//       setState(() {
+//         if (this.mounted) _pollList.addAll(pollList);
+//       });
+//     } catch (e) {
+//       print(e);
+//     }
+//   }
 
 /// Prepares chart info
   _getChartData(List<PollOption> options) {
@@ -98,26 +97,29 @@ class _PollListState extends State<PollList> {
 
   @override
   Widget build(BuildContext context) {
+    final pollStore = Provider.of<PollStore>(context);
+    final pollList = pollStore.polls;
+
     return ListView.separated(
       separatorBuilder: (context, index) => Divider(
         height: 4.0,
         color: Colors.grey,
       ),
-      itemCount: _pollList.length,
+      itemCount: pollList.length,
       itemBuilder: (context, index) {
         return ListTile(
           contentPadding: EdgeInsets.only(left: 10.0),
           title: Text(
-            '${_pollList[index].title}',
+            '${pollList[index].title}',
             overflow: TextOverflow.ellipsis,
           ),
           subtitle: Text(
-            '${_pollList[index].createdBy} - ${_getTotalVotes(_pollList[index].options)} Votes',
+            '${pollList[index].createdBy} - ${_getTotalVotes(pollList[index].options)} Votes',
           ),
-          trailing: _pollList[index].voted == true
+          trailing: pollList[index].voted == true
               ? AnimatedCircularChart(
                   size: const Size(80.0, 80.0),
-                  initialChartData: _getChartData(_pollList[index].options),
+                  initialChartData: _getChartData(pollList[index].options),
                   chartType: CircularChartType.Pie,
                   duration: Duration(
                     seconds: 1,
@@ -135,11 +137,11 @@ class _PollListState extends State<PollList> {
                   ),
                 ),
           onTap: () {
-            _pollList[index].voted == true
+            pollList[index].voted == true
                 ? Navigator.pushNamed(context, PollResultScreen.id,
-                    arguments: _pollList[index])
+                    arguments: pollList[index])
                 : Navigator.pushNamed(context, PollVoteScreen.id,
-                    arguments: _pollList[index]);
+                    arguments: pollList[index]);
           },
         );
       },
