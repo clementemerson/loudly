f7cf80ce67b79f584a058d946b29570ea4231be5
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-08-29 18:02:03 +0530

    Add files via upload

---
+++ lib/ui/widgets/toast_generator.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class ToastGenerator {
+  static Future<bool> showCenterToast(String msg) {
+    return Fluttertoast.showToast(
+        msg: msg,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIos: 1,
+        fontSize: 16.0);
+  }
+
+  static Future<bool> showBottomToast(String msg) {
+    return Fluttertoast.showToast(
+        msg: msg,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIos: 1,
+        fontSize: 16.0);
+  }
+
+  static Future<bool> showCenterErrorToast(String msg) {
+    return Fluttertoast.showToast(
+        msg: msg,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIos: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+
+  static Future<bool> showBottomErrorToast(String msg) {
+    return Fluttertoast.showToast(
+        msg: msg,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIos: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+
+  static void cancelToast() {
+    Fluttertoast.cancel();
+  }
+}
